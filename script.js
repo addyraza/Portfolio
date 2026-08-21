9a653d2e5e9070ebfb3f2568df6438a73e36e84d
addyraza <shadabraza9058@gmail.com>
2026-08-21 20:45:21 +0530

    add required attribute to message textarea and fix script tag syntax

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-var tablinks = document.getElementsByClassName('tab-links');
-var tabcontents = document.getElementsByClassName('tab-contents');
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(info){
-   for(tabname of tablinks)
-   {
-      tabname.classList.remove('active-link');
-   }
-   for(content of tabcontents)
-   {
-      content.classList.remove('active-tab');
-   }
-   event.currentTarget.classList.add('active-link');
-   document.getElementById(info).classList.add('active-tab');
+function opentab(info) {
+  for (tabname of tablinks) {
+    tabname.classList.remove("active-link");
+  }
+  for (content of tabcontents) {
+    content.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(info).classList.add("active-tab");
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwixHO6PDkZVjJk--7c5xaAYof1GMmKPYVsW5BGlmgtfLE_ivAdUrBvdnGdp04F6Qr7/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById('msg')
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyKyVt_WDhuQ8F6JUajANnIBjzH1JRaqKXzcPdAbA8pnHGNKPq_BUWMOD3qEck2mDe9/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-         msg.innerHTML = 'Message sent successfully!!'
-         setTimeout(function(){
-            msg.innerHTML = ''
-         },5000)
-         form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
-  form.reset();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully!!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+form.reset();
